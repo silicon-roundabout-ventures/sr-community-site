52e228384292841c9ffae7e4fd7d1780f536d020
Francesco Perticarari <23483149+fpert041@users.noreply.github.com>
2022-02-26 21:50:18 +0000

    readme restructured

---
+++ styles/themes/lightThemesOptions.ts
@@ -0,0 +1,21 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+const lightThemeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+  },
+};
+export default lightThemeOptions;
+
+/* starting point:
+
+ import { ThemeOptions } from '@mui/material/styles';
+
+ const lightThemeOptions: ThemeOptions = {
+   palette: {
+     mode: 'light',
+   },
+ };
+  export default lightThemeOptions;
+
+  */
